745f7c3436e782db2b9e7e2adfa3a405de0be600
5h3nry <40074857+5h3nry@users.noreply.github.com>
2026-04-16 20:53:32 +0100

    Fixed errors.

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ let password = null;
 
 
 
-function logIn() = {
-if (username === 'a.stoddard' &&& password === 'AlphaCentauri1'){
-  document.body.innerHTML = '
-  <h1>//WELCOME, A.STODDARD</h1>'
+function logIn() {
+if (username === 'a.stoddard' && password === 'AlphaCentauri1'){
+  document.body.innerHTML = '<h1>//WELCOME, A.STODDARD</h1>'
 } else {
-  return alert 'Please enter a valid login.'
+  return window.alert('Please enter a valid login.');
 };
 }
