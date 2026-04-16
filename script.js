88166bd3535a0b017a538915ebf30830f2f8a8b5
5h3nry <40074857+5h3nry@users.noreply.github.com>
2026-04-16 13:05:07 +0100

    updated javascript

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-$(document).ready(function () {
-  $("#title").focus();
-  $("#text").autosize();
-});
+if (document.getElementById('user').value === 'a.stoddard' &&& document.getElementById('pass') === 'AlphaCentauri1'){
+  document.body.innerHTML = '
+  <h1>THIS IS A TEST</h1>'
+} else {
+  return alert 'Please enter a valid login.'
+};
