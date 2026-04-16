b7add656976db828281253d624e1dae85d97496f
5h3nry <40074857+5h3nry@users.noreply.github.com>
2026-04-16 20:56:19 +0100

    Formatted javascript.

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let username = null;
 let password = null;
 
-
-
 function logIn() {
-if (username === 'a.stoddard' && password === 'AlphaCentauri1'){
-  document.body.innerHTML = '<h1>//WELCOME, A.STODDARD</h1>'
-} else {
-  return window.alert('Please enter a valid login.');
-};
+  if (username === "a.stoddard" && password === "AlphaCentauri1") {
+    document.body.innerHTML = "<h1>//WELCOME, A.STODDARD</h1>";
+  } else {
+    return window.alert("Please enter a valid login.");
+  }
 }
