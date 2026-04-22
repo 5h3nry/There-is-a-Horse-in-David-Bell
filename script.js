2583ef10fae4aa02b036096020156c2afe541106
5h3nry <40074857+5h3nry@users.noreply.github.com>
2026-04-22 01:16:14 +0100

    Updated form to go red when incorrect.

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const state = {
   user: null,
   isLoading: false,
-}
+};
 
 function logIn() {
-  let user = document.getElementById('userInput').value
-  let pass = document.getElementById('passInput').value
+  let user = document.getElementById("userInput").value;
+  let pass = document.getElementById("passInput").value;
 
-    if(user === "a.stoddard" && pass === "AlphaCentauri1") {
-      document.getElementById('invalid').innerHTML = ''
-      user = 'a.stoddard';
-
-    } else {
-      event.preventDefault;
-    alert('Please enter a valid login.');
-    document.getElementById('invalid').innerHTML = '<span>INVALID LOGIN</span>'
+  if (user === "a.stoddard" && pass === "AlphaCentauri1") {
+    document.getElementById("invalid").innerHTML = "";
+    user = "a.stoddard";
+  } else {
+    event.preventDefault;
+    document.getElementById("userInput").classList.add("invalid");
+    document.getElementById("passInput").classList.add("invalid");
+    document.getElementById("userInput").addEventListener("animationend", (event) => { document.getElementById("userInput").classList.remove("invalid");})
+    document.getElementById("passInput").addEventListener("animationend", (event) => { document.getElementById("passInput").classList.remove("invalid");})
+    document.getElementById("invalid").innerHTML = "<span>INVALID LOGIN</span>";
+    document.getElementById("logIn").reset();
   }
-};
+}
